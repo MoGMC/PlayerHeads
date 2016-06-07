4979cffc0d84f9ef7c5b2959a3d4f7c18575d798
seawee65 <seawee65@live.com>
2016-06-06 21:54:51 -0400

    fixed importing remote files

--- src/net/hilaryoi/plugin/headplugin/HeadPlugin.java
+++ src/net/hilaryoi/plugin/headplugin/HeadPlugin.java
@@ -1,11 +1,33 @@
 package net.hilaryoi.plugin.headplugin;
 
+import java.io.IOException;
+
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class HeadPlugin extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+
+		try {
+
+			HeadFactory.loadHeads("https://raw.githubusercontent.com/MoGMC/PlayerHeads/master/heads.yml");
+			CrateFactory.loadCrates("https://raw.githubusercontent.com/MoGMC/PlayerHeads/master/crates.yml");
+
+		} catch (IOException e) {
+
+			Bukkit.getLogger().severe("Could not fetch head or crate information. Disabling plugin.");
+			Bukkit.getPluginManager().disablePlugin(this);
+
+			e.printStackTrace();
+
+		}
+
+	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
